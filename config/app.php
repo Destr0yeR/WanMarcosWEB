325af46404557942d8a1bcf157fa8a082c6d2c61
Javier Palomino <jfpp92@gmail.com>
2015-09-11 12:01:18 -0500

    added configuration for JWTAuth

--- config/app.php
+++ config/app.php
@@ -145,6 +145,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Packages Service Provider
+         */
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider:class,
     ],
 
     /*
@@ -194,6 +198,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
 
+        'JWTAuth'       => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory'    => Tymon\JWTAuth\Facades\JWTFactory::class,
     ],
 
 ];
