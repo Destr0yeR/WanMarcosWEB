febe2dad678738e93d3f1a944da1da5001d2aee3
Javier Palomino <jfpp92@gmail.com>
2015-12-04 12:47:39 -0500

    added faculty and degree users/me

--- app/Services/API/User/UserFormater.php
+++ app/Services/API/User/UserFormater.php
@@ -4,7 +4,15 @@ namespace App\Services\API\User;
 
 use App\Services\Interfaces\Formater;
 
+use App\Repositories\FacultyRepository;
+use App\Repositories\DegreeRepository;
+
 class UserFormater implements Formater {
+
+    public function __construct(){
+        $this->faculty_repository = new FacultyRepository;
+        $this->degree_repository = new DegreeRepository;
+    }
 
     public function format($users){
         
@@ -27,6 +35,24 @@ class UserFormater implements Formater {
             $_user['image'] = asset($user->image);
         }
         else $_user['image'] = null;
+
+        if($user->faculty_id){
+            $faculty = $this->faculty_repository->find($user->faculty_id);
+            $_user['faculty'] = [
+                'id'    => $faculty->id,
+                'name'  => $faculty->name
+            ];
+        }
+        else $_user['faculty'] = null;
+
+        if($user->degree_id){
+            $degree = $this->degree_repository->find($user->degree_id);
+            $_user['degree'] = [
+                'id'    => $degree->id,
+                'name'  => $degree->name
+            ];
+        }
+        else $_user['degree'] = null;
 
         return $_user;
     }
